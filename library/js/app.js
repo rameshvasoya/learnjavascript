5987f9389f8e4e9f8e9ae1703efc08536aed4933
rameshsit <55971103+rameshsit@users.noreply.github.com>
2022-01-19 00:58:14 +0530

    library exercise continues

--- library/js/app.js
+++ library/js/app.js
@@ -5,26 +5,42 @@ function Book(name, author, type) {
 }
 
 // Display Construction
-function Display() {
- 
-}
+function Display() {}
 
 // add methods to display prototype
-Display.prototype.add = function(book) {
-  console.log('add to ui')
-  tableBody = document.getElementById('tableBody');
+Display.prototype.add = function (book) {
+  console.log("add to ui");
+  tableBody = document.getElementById("tableBody");
   let uiString = `<tr>
                     <td>${book.name}</td>
                     <td>${book.author}</td>
                     <td>${book.type}</td>
                     </tr>`;
-  tableBody.innerHtml = uiString;
+  tableBody.innerHtml += uiString;
 }
 
-Display.prototype.clear = function() {
+// implement the clear function
+Display.prototype.clear = function () {
   let libraryForm = document.getElementById("libraryForm");
   libraryForm.reset();
 }
+
+// implement the valitedate function
+Display.prototype.validate = function (book) {
+  if (book.name.length < 2 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+Display.prototype.show = function (type, displayMessage) {
+  let message = document.getElementById("alertsMsg");
+  message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                          <strong>Message:</strong> ${displayMessage}
+                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>`;
+};
 
 // Add Submit event listener to libraryForm
 let libraryForm = document.getElementById("libraryForm");
@@ -52,8 +68,15 @@ function libraryFormsubmit(e) {
   let book = new Book(name, author, type);
   console.log(book);
 
-  let display = new Display()
-  display.add(book);
-  display.clear();
+  let display = new Display();
+  if (display.validate(book)) {
+    display.add(book);
+    display.clear();
+    display.show("success", "Your book has been successfully added.");
+  } else {
+    display.show("danger", "Sorry you cannot add this book.");
+  }
+
+
   e.preventDefault();
 }
